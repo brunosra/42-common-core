c5022b5f76af1c500c0f8ebd2f4f4e874a6e6277
Bruno S. Almeida <bschwell@Wqstudent.42porto.com>
2024-10-08 11:46:48 +0100

    finish so_long

--- rank02/so_long/src/graphs/ft_put_tile.c
+++ rank02/so_long/src/graphs/ft_put_tile.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_put_tile.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bschwell <student@42.fr>                   +#+  +:+       +#+        */
+/*   By: bschwell <bschwell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 09:09:23 by bschwell          #+#    #+#             */
-/*   Updated: 2024/10/07 16:00:29 by bschwell         ###   ########.fr       */
+/*   Updated: 2024/10/08 11:43:55 by bschwell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ void	ft_put_tile(t_game *game, char *path, int x, int y)
 	img = mlx_xpm_file_to_image(game->mlx, path,
 			&game->sprite_size, &game->sprite_size);
 	if (img == NULL)
+	{
+		ft_print_err(ERROR_IMG_LOAD);
 		ft_free_game_and_exit(game);
+	}
 	mlx_put_image_to_window(game->mlx, game->win, img, x, y);
 	mlx_destroy_image(game->mlx, img);
 }
--- rank02/so_long/src/map_checker/ft_is_collectible.c
+++ rank02/so_long/src/map_checker/ft_is_collectible.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_is_collectible.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bschwell <student@42.fr>                   +#+  +:+       +#+        */
+/*   By: bschwell <bschwell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 23:09:28 by bschwell          #+#    #+#             */
-/*   Updated: 2024/10/07 11:29:20 by bschwell         ###   ########.fr       */
+/*   Updated: 2024/10/08 11:41:27 by bschwell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ int	ft_is_collectible(t_map *map)
 	}
 	if (c == 0)
 	{
-		ft_print_err("Map has no collectibles");
+		ft_print_err(ERROR_COLLECT);
 		return (0);
 	}
-	ft_print_suc("Map has at least one collectible!");
+	ft_print_suc(ERROR_COLLECT);
 	return (1);
 }
