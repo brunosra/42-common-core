5282a713c0a6ae27b7595f10500619cd89265b40
Bruno S. Almeida <bschwell@Wqstudent.42porto.com>
2024-11-20 17:09:56 +0000

    add philo philo

---
+++ rank03/philo/philo/philo.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <limits.h>
+#include "colors.h"
+
+/* Strucs */
+typedef pthread_mutex_t	t_mtx;
+
+typedef struct s_fork
+{
+	t_mtx	fork;
+	int		fork_id;
+}	t_fork;
+
+/* Test 1 800 200 200. The philosopher should not eat and should die.
+Test 5 800 200 200. No philosopher should die.
+Test 5 800 200 200 7. No philosopher should die and the simulation should stop when every philosopher has eaten at least 7 times.
+Test 4 410 200 200. No philosopher should die.
+Test 4 310 200 100. One philosopher should die. */
+
+typedef struct s_philo
+{
+	int			id;
+	long		meals_counter;
+	bool		full;
+	long		last_meal_time;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id;
+	t_table		*table;
+}	t_philo;
+
+typedef struct s_table
+{
+	long	philo_nbr;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	nbr_limit_meals;
+	long	start_simulation;
+	bool	end_simulation;
+	t_fork	*forks;
+	t_philo	*philos;
+}	t_table;
