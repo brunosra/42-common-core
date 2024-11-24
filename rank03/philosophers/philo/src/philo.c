f657b2701868a49a87b5b300df8aa72d75b8ead6
Bruno S. Almeida <bschwell@Wqstudent.42porto.com>
2024-11-24 18:26:40 +0000

    add marsoares minishell and philo

--- rank03/philosophers/philo/src/philo.c
+++ rank03/philosophers/philo/src/philo.c
@@ -6,11 +6,36 @@
 /*   By: bschwell <bschwell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 20:11:27 by bschwell          #+#    #+#             */
-/*   Updated: 2024/11/24 13:45:05 by bschwell         ###   ########.fr       */
+/*   Updated: 2024/11/24 15:26:14 by bschwell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static int	ft_isnum(char c)
+{
+	if (c < '0' || c > '9')
+		return (0);
+	return (1);
+}
+
+static int	ft_check_args(char **argv)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (argv[++i])
+	{
+		j = -1;
+		while (argv[i][++j])
+		{
+			if (ft_isnum(argv[i][j]) == 0)
+				return (0);
+		}
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
@@ -21,6 +46,11 @@ int	main(int argc, char **argv)
 		ft_throw_error("Usage: philos_nbr "
 			"time_to_die time_to_eat time_to_sleep "
 			"[meals_nbr]", NULL, 0);
+		return (0);
+	}
+	if (ft_check_args(argv) == 0)
+	{
+		ft_throw_error("Only pure positive numbers are accepted", NULL, 0);
 		return (0);
 	}
 	if (ft_parse_args_to_table(&table, argv) == 0)
